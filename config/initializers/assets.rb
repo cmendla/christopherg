ed7f7553892d716a1f9c9a966695f073b2ff1911
Christopher G Mendla <chrismendla@gmail.com>
2017-10-03 15:09:52 -0400

    Migration to Rails 5

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-# Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '2.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -10,8 +10,7 @@
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-
-#   Rails.application.config.assets.compile = true                                    #for Production
+  # Rails.application.config.assets.compile = true                                    #for Production
   Rails.application. config.assets.precompile = ['*.js', '*.css', '*.css.erb']      #for Production
   Rails.application.config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]        #for Production
   Rails.application.config.assets.precompile += %w(script.js)
@@ -19,6 +18,3 @@
   Rails.application.config.assets.precompile += %w( style.css )
   Rails.application.config.assets.precompile += %w( style.responsive.css )
   Rails.application.config.assets.precompile += %w( style.ie7.css )
-
- # Rails.application.config.assets.precompile += %w(ckeditor/*)
- # Rails.application.config.assets.precompile += %w(jquery.are-you-sure.js)
