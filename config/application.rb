3e59d41e95e51da7019ec4cf37f6731cf0dcb3ba
Christopher G Mendla <chrismendla@gmail.com>
2018-01-23 12:15:02 -0500

    Changes to content of portfolio
    Signed-off-by: Christopher G Mendla <chrismendla@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ module RailsProject
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    
+    # following line is not valid in rails 5
+    # config.active_record.raise_in_transactional_callbacks = true
+ 
+ 
   end
 end
