dd26387120ffe58e812d7b2eb16f96119811ffff
Chris Mendla <chrismendla@gmail.com>
2020-09-22 22:27:14 -0400

    modify unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,32 @@
+# set path to the application
+app_dir = File.expand_path("../..", __FILE__)
+app_path = File.expand_path(File.dirname(__FILE__) + '/..')
+shared_dir = "#{app_dir}/shared"
+working_directory app_dir
+# require 'rvm'
+# RVM.use_from_path! File.dirname(File.dirname(__FILE__))
+# gem_path = ~/home/cgmadmin/.rvm/rubies/ruby-2.7.1/bin/ruby
+# gem_home = ~/home/cgmadmin/.rvm/rubies
+# Set unicorn options
+worker_processes 2
+preload_app true
+timeout 30
+puts "--------------------"
+puts app_dir
+puts app_path
+puts shared_dir
+# puts gem_path
+# puts gem_home
+puts "------------------------------"
+
+
+# Path for the Unicorn socket
+listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+
+# Set path for logging
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+
+# Set proccess id path
+pid "#{shared_dir}/pids/unicorn.pid"
+
