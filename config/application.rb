a026a0093d82b1b5f05e5ac86a84999ae64797fd
Chris Mendla <chrismendla@gmail.com>
2020-11-18 12:06:41 -0500

    Update to Rails 5.2

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,11 @@ Bundler.require(*Rails.groups)
 module RailsProject
   class Application < Rails::Application
     config.generators.javascript_engine = :js # Switch from coffeescript to js
+    config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
